1b6dc5ce3f349cbee84b0b21143d2f7972a82a3c
Thanh Binh Do <thanhbinhwork5286@gmail.com>
2026-08-15 16:27:57 +0700

    feat: update router configuration to use hash location strategy

--- QuanLyNhaHang_Admin/src/app/app.config.ts
+++ QuanLyNhaHang_Admin/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -36,7 +36,7 @@ export const appConfig: ApplicationConfig = {
       withFetch(),
       withInterceptors([authInterceptorFn])
     ),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
